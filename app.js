a8e9a1200baead784d12b853a388fce4b952c6e6
Ignacio Martin Elias <ignaciomartinelias@gmail.com>
2019-07-20 19:06:01 -0300

    add cross-env npm scripts

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
 // Requiering Dependencies, Models, Seeds & Routes
-
-var dotEnv = require('dotenv');
-dotEnv.config();
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
 
 var express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser'),
 	mongoose = require('mongoose'),
-	expressSession = require("express-session"),
+	expressSession = require('express-session'),
 	passport = require('passport'),
-	methodOverride = require("method-override"),
-	moment = require("moment"),
+	methodOverride = require('method-override'),
+	moment = require('moment'),
 	LocalStrategy = require('passport-local'),
 	User = require('./models/user'),
 	seedDB = require('./seeds'),
-	flash = require("connect-flash"),
-	indexRoutes = require("./routes/routes-index"),
-	campgroundRoutes = require("./routes/routes-campgrounds"),
-	commentRoutes = require("./routes/routes-comments");
-
+	flash = require('connect-flash'),
+	indexRoutes = require('./routes/routes-index'),
+	campgroundRoutes = require('./routes/routes-campgrounds'),
+	commentRoutes = require('./routes/routes-comments');
 
 // Seed DB
 // seedDB();
 
 // APP CONFIGURATION
 
-mongoose.connect(process.env.MONGODB_URL , { useNewUrlParser: true , useFindAndModify: false});
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useFindAndModify: false });
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 app.use(flash());
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(expressSession({
-	secret: "Colabore para no desaparecer",
-	resave: false,
-	saveUninitialized: false
-}));
+app.use(
+	expressSession({
+		secret: 'Colabore para no desaparecer',
+		resave: false,
+		saveUninitialized: false
+	})
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -48,19 +49,19 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req,res,next){
+app.use(function(req, res, next) {
 	res.locals.currentUser = req.user;
-	res.locals.error = req.flash("error");
-	res.locals.success = req.flash("success");
+	res.locals.error = req.flash('error');
+	res.locals.success = req.flash('success');
 	res.locals.moment = moment;
 	next();
-})
+});
 
 app.use(indexRoutes);
-app.use("/campgrounds" , campgroundRoutes);
-app.use("/campgrounds/:id/comments" , commentRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/comments', commentRoutes);
 
 // SERVER
-app.listen(process.env.PORT , process.env.IP, function() {
+app.listen(process.env.PORT, process.env.IP, function() {
 	console.log('Yelpcamp Server Started!!');
 });
